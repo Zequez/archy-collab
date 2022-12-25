f17161bca68f1698934f46db3bb9dad8bf0f32b9
Zequez <zequez@gmail.com>
2022-12-25 19:38:31 -0300

    Moved colors to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,13 +3,19 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				kalam: ["Kalam", ...defaultTheme.fontFamily.sans]
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        kalam: ["Kalam", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        esgreen1: "#90b461",
+        esgreen2: "#53c8aa",
+        espurple1: "#b461b2",
+        espurple2: "#8561B4",
+      },
+    },
+  },
+  plugins: [],
+};
