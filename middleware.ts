9da1ce2bc5cbc1415ab18e308c98bc0043901a22
Zequez <zequez@gmail.com>
2023-02-16 11:54:46 -0300

    Fixed the domains re-writing system

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ const PROD_HOST = "archy.site";
 export const hostMap: { [key: string]: string } = {
   // 'archy.site': '',
   // 'localhost:3000': '',
+  "ezequielschwartzman.local:3000": "ezequiel",
   "ezequielschwartzman.org": "ezequiel",
   "nickfeint.com": "nick",
   "eltemplo.com": "templo.nach",
@@ -27,19 +28,6 @@ const baseHost =
     ? PROD_HOST
     : DEV_HOST;
 
-// const modules = ['cv', 'links', 'songs'];
-
-function replaceFirstHostMap() {}
-
-// ezequielschwartzman.org => /sites/links/ezequiel"
-// cv.ezequiel => /sites/cv/ezequiel
-// "nickfeint.com => "/sites/links"
-
-/*
-if url is on full hosts mapping replace whole url and
-
-*/
-
 export const config = {
   matcher: [
     /*
@@ -51,13 +39,35 @@ export const config = {
   ],
 };
 
-function removeBaseSharedHost(fullHostname: string): string {
+export function removeBaseSharedHost(fullHostname: string): string {
   return fullHostname.replace(new RegExp(`.${baseHost}$`), "");
 }
 
-function rewriteIndependentHostWishSharedHost(anyHost: string): string {
-  return hostMap[anyHost] || anyHost;
+// wall.ezequielschwartzman.org => wall.ezequiel.archy.site | wall.ezequiel.localhost:3000
+export function rewriteIndependentHostWishSharedHost(anyHost: string): string {
+  for (let host in hostMap) {
+    if (anyHost.endsWith(host)) {
+      const subdomains = anyHost.slice(0, -host.length);
+      const localAgent = hostMap[host];
+      return `${subdomains}${localAgent}.${baseHost}`;
+    }
+  }
+
+  return anyHost;
+  // return hostMap[anyHost] || anyHost;
 }
+
+rewriteIndependentHostWishSharedHost.test = () => {
+  // With baseHost = localhost:3000
+  console.info("rewriteIndependentHostWishSharedHost");
+  let tf = rewriteIndependentHostWishSharedHost;
+  // Phase 1
+  assert(tf("ezequielschwartzman.local:3000"), "ezequiel.localhost:3000");
+  assert(tf("ezequielschwartzman.org"), "ezequiel.localhost:3000");
+  assert(tf("nickfeint.com"), "nick.localhost:3000");
+  // Phase 2
+  assert(tf("walls.ezequielschwartzman.org"), "wall.ezequiel.localhost:3000");
+};
 
 export default function middleware(req: Request) {
   const url = new URL(req.url);
@@ -121,3 +131,27 @@ function responseRedirect(url: string) {
   headers.set("Location", validateURL(url));
   return new Response(null, { headers });
 }
+
+// ████████╗███████╗███████╗████████╗███████╗
+// ╚══██╔══╝██╔════╝██╔════╝╚══██╔══╝██╔════╝
+//    ██║   █████╗  ███████╗   ██║   ███████╗
+//    ██║   ██╔══╝  ╚════██║   ██║   ╚════██║
+//    ██║   ███████╗███████║   ██║   ███████║
+//    ╚═╝   ╚══════╝╚══════╝   ╚═╝   ╚══════╝
+
+// I actually have an emotional reaction with the terms PASS and FAIL, so I prefer to use FLOW and WALL.
+function assert<T>(val1: T, val2: T) {
+  // console.info("-- ")
+  if (val1 === val2) {
+    console.info("\x1b[36m\x1b[44mFLOW\x1b[0m", val1);
+  } else {
+    console.error("\x1b[37m\x1b[40mWALL\x1b[0m", val1, val2);
+  }
+}
+
+(async () => {
+  if (process.env.NODE_ENV !== "production") {
+    console.info("\x1b[42m\x1b[30m[TEST] Middleware\x1b[0m");
+    rewriteIndependentHostWishSharedHost.test();
+  }
+})();
