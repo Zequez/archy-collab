d8d3783014d68418576ec79a99ddea8866e80a14
Zequez <zequez@gmail.com>
2023-03-30 13:07:24 -0300

    Show editable class and not-yet-editable attributes

--- editor/src/HTMLTreeEditor.tsx
+++ editor/src/HTMLTreeEditor.tsx
@@ -51,6 +51,68 @@ const HTMLTreeEditor = ({ value, onChange }: HTMLTreeEditorProps) => {
     serialize();
   }
 
+  function changeAttributeAndRefresh(
+    node: Element,
+    attrName: string,
+    newVal: string
+  ) {
+    node.setAttribute(attrName, newVal);
+    setEdits(edits + 1);
+    serialize();
+  }
+
+  const renderAttributes = (node: Element) => {
+    const attributes = Array.from(node.attributes).filter(
+      ({ name }) => name !== "class"
+    );
+    return (
+      <div>
+        {attributes.map(({ name, value }) => (
+          <div className="text-xs text-center whitespace-nowrap font-mono">
+            <div
+              contentEditable="true"
+              suppressContentEditableWarning={true}
+              className="bg-red-500/25 px-1 rounded-t-md"
+            >
+              {name}
+            </div>
+            <div
+              contentEditable={true}
+              suppressContentEditableWarning={true}
+              className="bg-red-500/10 px-1 rounded-b-md"
+            >
+              {value}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+    // for (let i = 0; i < attributes.length; ++i) {
+    //   const { name, value } = attributes.item(i) as Attr ;
+    //   if (name !== 'class') {
+
+    //   }
+    // }
+  };
+
+  const STYLELESS_NODES = ["meta", "title", "script", "link", "head"];
+
+  const renderClass = (node: Element) => {
+    const noClass = STYLELESS_NODES.includes(node.tagName.toLowerCase());
+    return !noClass ? (
+      <TextareaAutosize
+        className="bg-cyan-400/25 min-h-full resize-none rounded-md border border-solid border-black/10 ml-0.5 px-2 py-1 m-0 flex-grow"
+        style={{ lineHeight: "26px" }}
+        placeholder="Style directives"
+        minRows={1}
+        value={node.getAttribute("class") || ""}
+        onChange={(ev) =>
+          changeAttributeAndRefresh(node, "class", ev.target.value)
+        }
+      />
+    ) : null;
+  };
+
   const renderNode = (node: Element) => {
     const children = Array.from(node.childNodes).filter(
       (child) =>
@@ -60,21 +122,27 @@ const HTMLTreeEditor = ({ value, onChange }: HTMLTreeEditorProps) => {
     ) as Element[];
 
     return (
-      <div className="pl-4">
+      <div className="pl-4 border-l border-l-yellow-400/10 border-solid">
         <div className="flex">
           {node.nodeType === Node.ELEMENT_NODE ? (
-            <div className="flex flex-grow">
+            <div className="flex flex-grow my-0.25">
               <input
-                type="text"
-                className="font-mono font-bold flex-grow px-2 py-1 my-0.25 bg-white/50 border border-solid border-black/10 rounded-md focus:outline outline-solid-green-500"
+                className="block w-20 flex-shrink flex items-center font-mono font-bold px-2 py-1 bg-white/50 border border-solid border-black/10 rounded-md focus:outline outline-solid-green-500 shadow-sm"
+                // contentEditable={true}
+                // suppressContentEditableWarning={true}
                 value={node.tagName.toLowerCase()}
                 onChange={(ev) => renameNodeAndRefresh(node, ev.target.value)}
+                // onInput={(ev) =>
+                //   renameNodeAndRefresh(node, ev.currentTarget.innerText)
+                // }
               />
+              {renderAttributes(node)}
+              {renderClass(node)}
             </div>
           ) : node.nodeType === Node.TEXT_NODE ? (
             <TextareaAutosize
               minRows={1}
-              className="text-gray-600 bg-white px-2 py-1 flex-grow resize-none focus:outline outline-solid-green-500"
+              className="text-gray-600 font-sans bg-white px-2 py-1 flex-grow resize-none border border-solid border-black/10 focus:outline outline-solid-green-500 rounded-sm"
               value={node.textContent || ""}
               onChange={(ev) => changeTextAndRefresh(node, ev.target.value)}
             />
