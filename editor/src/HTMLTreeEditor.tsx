1d8a21da55839ae411af53c97fcb1c4a50168bbc
Zequez <zequez@gmail.com>
2023-04-01 03:01:36 -0300

    Track element being edited and some clean up refactoring

--- editor/src/HTMLTreeEditor.tsx
+++ editor/src/HTMLTreeEditor.tsx
@@ -60,7 +60,12 @@ const HTMLTreeEditor = ({ value, onChange }: HTMLTreeEditorProps) => {
   }
 
   function renameNode(node: Element, newNodeName: string) {
-    N.renameNode(node, newNodeName);
+    const newNode = N.renameNode(node, newNodeName);
+    console.log("Renaming node!");
+    if (editingNode === node) {
+      console.log("Editing node is the one being renamed");
+      setEditingNode(newNode);
+    }
     refresh();
   }
 
@@ -99,7 +104,105 @@ const HTMLTreeEditor = ({ value, onChange }: HTMLTreeEditorProps) => {
     refresh();
   }
 
-  const ATTRIBUTES_NOT_TO_RENDER = ["class", "style", "data-collapsed"];
+  return (
+    <div className="bg-yellow-50 p-1">
+      <HTMLNode
+        node={rootNode}
+        editingNode={editingNode}
+        onFocus={(node) => setEditingNode(node)}
+        onSetTagName={renameNode}
+        onSetAttribute={changeAttribute}
+        onSetStyleDirectives={changeStyleDirectives}
+        onSetText={changeText}
+      />
+    </div>
+  );
+};
+
+const renderableChildNodes = (node: Element, editingNode: Element | null) => {
+  const childNodes = Array.from(node.childNodes).filter(
+    (child) =>
+      child.nodeType === Node.ELEMENT_NODE ||
+      (child.nodeType === Node.TEXT_NODE &&
+        (!isBlank(child.textContent) || child === editingNode))
+  );
+  return childNodes as Element[];
+};
+
+const nodeIsCollapsed = (node: Element) =>
+  node.nodeType === Node.ELEMENT_NODE
+    ? node.getAttribute("data-collapsed") !== null
+    : false;
+
+const generateNodeStyleDirective = (node: Element): string => {
+  const classes = node.getAttribute("class");
+  const styles = node.getAttribute("style");
+  if (classes) {
+    if (styles && classes.match("{}")) {
+      return classes.replace("{}", `{${styles}}`);
+    } else {
+      return classes;
+    }
+  } else if (styles) {
+    return `{${styles}}`;
+  } else {
+    return "";
+  }
+};
+
+const STYLELESS_NODES = ["meta", "title", "script", "link", "head"];
+const ATTRIBUTES_NOT_TO_RENDER = ["class", "style", "data-collapsed"];
+
+type HTMLNodeProps = {
+  node: Element;
+  editingNode: Element | null;
+  onFocus: (node: Element) => void;
+  onSetAttribute: (node: Element, attr: string, val: string | boolean) => void;
+  onSetTagName: (node: Element, newName: string) => void;
+  onSetStyleDirectives: (node: Element, styleDirective: string) => void;
+  onSetText: (node: Element, text: string) => void;
+};
+
+const HTMLNode = ({
+  node,
+  editingNode,
+  onFocus,
+  onSetAttribute,
+  onSetTagName,
+  onSetStyleDirectives,
+  onSetText,
+}: HTMLNodeProps) => {
+  const children = renderableChildNodes(node, editingNode);
+  const isEditing = node === editingNode;
+
+  const renderToggle = (node: Element, children: Element[]) => {
+    const isCollapsed = nodeIsCollapsed(node);
+    return children.length ? (
+      <button
+        className={cx(
+          "*flex-vh *b1 w-4 h-4 px-0.5 -ml-4 border-yellow/10 rounded-sm mr-0.25 text-white cursor-pointer",
+          { "bg-yellow-700/50": !isCollapsed, "bg-black": isCollapsed }
+        )}
+        onClick={() => onSetAttribute(node, "data-collapsed", !isCollapsed)}
+      >
+        {isCollapsed ? children.length : <CaretUp />}
+      </button>
+    ) : null;
+  };
+
+  const renderStyleDirective = (node: Element) => {
+    const noClass = STYLELESS_NODES.includes(node.tagName.toLowerCase());
+    return !noClass ? (
+      <TextareaAutosize
+        className="bg-cyan-400/25 min-h-full resize-none rounded-md *b1 border-black/10 ml-0.5 px-2 py-2 m-0 flex-grow first:mt-0 mt-0.5 text-xs"
+        style={{ lineHeight: "14px" }}
+        placeholder="Style directives"
+        minRows={1}
+        value={generateNodeStyleDirective(node)}
+        onChange={(ev) => onSetStyleDirectives(node, ev.target.value)}
+      />
+    ) : null;
+  };
 
   const renderAttributes = (node: Element) => {
     const attributes = Array.from(node.attributes).filter(
@@ -108,7 +211,10 @@ const HTMLTreeEditor = ({ value, onChange }: HTMLTreeEditorProps) => {
     return attributes.length > 0 ? (
       <div className="flex flex-wrap ml-0.5">
         {attributes.map(({ name, value }) => (
-          <div className="text-xs text-center whitespace-nowrap font-mono mx-0.25">
+          <div
+            className="text-xs text-center whitespace-nowrap font-mono mx-0.25"
+            key={name}
+          >
             <div
               contentEditable="true"
               suppressContentEditableWarning={true}
@@ -129,113 +235,60 @@ const HTMLTreeEditor = ({ value, onChange }: HTMLTreeEditorProps) => {
     ) : null;
   };
 
-  const STYLELESS_NODES = ["meta", "title", "script", "link", "head"];
-
-  const generateNodeStyleDirective = (node: Element): string => {
-    const classes = node.getAttribute("class");
-    const styles = node.getAttribute("style");
-    if (classes) {
-      if (styles && classes.match("{}")) {
-        return classes.replace("{}", `{${styles}}`);
-      } else {
-        return classes;
-      }
-    } else if (styles) {
-      return `{${styles}}`;
-    } else {
-      return "";
-    }
-  };
-
-  const renderStyleDirective = (node: Element) => {
-    const noClass = STYLELESS_NODES.includes(node.tagName.toLowerCase());
-    return !noClass ? (
-      <TextareaAutosize
-        className="bg-cyan-400/25 min-h-full resize-none rounded-md *b1 border-black/10 ml-0.5 px-2 py-2 m-0 flex-grow first:mt-0 mt-0.5 text-xs"
-        style={{ lineHeight: "14px" }}
-        placeholder="Style directives"
-        minRows={1}
-        value={generateNodeStyleDirective(node)}
-        onChange={(ev) => changeStyleDirectives(node, ev.target.value)}
-      />
-    ) : null;
-  };
-
-  const nodeIsCollapsed = (node: Element) =>
-    node.nodeType === Node.ELEMENT_NODE
-      ? node.getAttribute("data-collapsed") !== null
-      : false;
-
-  const renderToggle = (node: Element, children: Element[]) => {
-    const isCollapsed = nodeIsCollapsed(node);
-    return children.length ? (
-      <button
-        className={cx(
-          "*flex-vh *b1 w-4 h-4 px-0.5 -ml-4 border-yellow/10 rounded-sm mr-0.25 text-white cursor-pointer",
-          { "bg-yellow-700/50": !isCollapsed, "bg-black": isCollapsed }
-        )}
-        onClick={() => changeAttribute(node, "data-collapsed", !isCollapsed)}
-      >
-        {isCollapsed ? children.length : <CaretUp />}
-      </button>
-    ) : null;
-  };
-
-  const renderableChildNodes = (node: Element) => {
-    const childNodes = Array.from(node.childNodes).filter(
-      (child) =>
-        child.nodeType === Node.ELEMENT_NODE ||
-        (child.nodeType === Node.TEXT_NODE &&
-          (!isBlank(child.textContent) || child === editingNode))
-    );
-    return childNodes as Element[];
-  };
-
-  const renderNode = (node: Element, i: number) => {
-    const children = renderableChildNodes(node);
-
-    return (
+  return (
+    <div className="pl-4 border-l border-l-yellow-400/10 border-solid">
       <div
-        className="pl-4 border-l border-l-yellow-400/10 border-solid"
-        key={i}
+        className="flex relative group"
+        // tabIndex={0}
+        onFocus={() => onFocus(node)}
       >
-        <div className="flex">
-          {node.nodeType === Node.ELEMENT_NODE ? (
-            <div className="*flex-v flex-grow my-0.25">
-              {renderToggle(node, children)}
-              <input
-                className="block h-full w-20 flex-shrink flex items-center font-mono font-bold px-2 py-1 bg-white/50 border border-solid border-black/10 rounded-md focus:outline outline-solid-green-500 shadow-sm"
-                // contentEditable={true}
-                // suppressContentEditableWarning={true}
-                value={node.tagName.toLowerCase()}
-                onChange={(ev) => renameNode(node, ev.target.value)}
-                // onInput={(ev) =>
-                //   renameNodeAndRefresh(node, ev.currentTarget.innerText)
-                // }
-              />
-              <div className="flex-grow flex flex-col">
-                {renderAttributes(node)}
-                {renderStyleDirective(node)}
-              </div>
-            </div>
-          ) : node.nodeType === Node.TEXT_NODE ? (
-            <TextareaAutosize
-              minRows={1}
-              className="text-gray-600 font-sans bg-white px-2 py-1 flex-grow resize-none border border-solid border-black/10 focus:outline outline-solid-green-500 rounded-sm"
-              value={node.textContent || ""}
-              onChange={(ev) => changeText(node, ev.target.value)}
+        {isEditing ? (
+          <div className="rounded-md bg-yellow-400/10 absolute inset-0 pointer-events-none"></div>
+        ) : null}
+        {node.nodeType === Node.ELEMENT_NODE ? (
+          <div className="*flex-v flex-grow my-0.25">
+            {renderToggle(node, children)}
+            <input
+              className="block h-full w-20 flex-shrink flex items-center font-mono font-bold px-2 py-1 bg-white/50 border border-solid border-black/10 rounded-md focus:outline outline-solid-green-500 shadow-sm"
+              // contentEditable={true}
+              // suppressContentEditableWarning={true}
+              value={node.tagName.toLowerCase()}
+              onChange={(ev) => onSetTagName(node, ev.target.value)}
+              // onInput={(ev) =>
+              //   renameNodeAndRefresh(node, ev.currentTarget.innerText)
+              // }
             />
-          ) : null}
-        </div>
-
-        {nodeIsCollapsed(node)
-          ? null
-          : children.map((child, i) => renderNode(child, i))}
+            <div className="flex-grow flex flex-col">
+              {renderAttributes(node)}
+              {renderStyleDirective(node)}
+            </div>
+          </div>
+        ) : node.nodeType === Node.TEXT_NODE ? (
+          <TextareaAutosize
+            minRows={1}
+            className="text-gray-600 font-sans bg-white px-2 py-1 flex-grow resize-none border border-solid border-black/10 focus:outline outline-solid-green-500 rounded-sm"
+            value={node.textContent || ""}
+            onChange={(ev) => onSetText(node, ev.target.value)}
+          />
+        ) : null}
       </div>
-    );
-  };
 
-  return <div className="bg-yellow-50 p-1">{renderNode(rootNode, 0)}</div>;
+      {nodeIsCollapsed(node)
+        ? null
+        : children.map((child, i) => (
+            <HTMLNode
+              key={i}
+              node={child}
+              editingNode={editingNode}
+              onFocus={onFocus}
+              onSetTagName={onSetTagName}
+              onSetAttribute={onSetAttribute}
+              onSetStyleDirectives={onSetStyleDirectives}
+              onSetText={onSetText}
+            />
+          ))}
+    </div>
+  );
 };
 
 export default HTMLTreeEditor;
